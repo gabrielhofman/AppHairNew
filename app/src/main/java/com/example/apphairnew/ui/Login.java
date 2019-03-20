da4b5afb00c56dd33b0db5a4c0890e0db5a61198
Gabriel Hofman <gabriel.hofman@hotmail.com>
2019-03-20 00:22:09 -0300

    test2

--- app/src/main/java/com/example/apphairnew/ui/Login.java
+++ app/src/main/java/com/example/apphairnew/ui/Login.java
@@ -1,6 +1,7 @@
 package com.example.apphairnew.ui;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -17,6 +18,17 @@ import android.widget.Toast;
 import com.example.apphairnew.MainActivity;
 import com.example.apphairnew.R;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.PropertyInfo;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpResponseException;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
 public class Login extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private Login login;
@@ -24,13 +36,19 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
     private EditText campoSenha;
     private String loginUsuario;
     private String senhaUsuario;
+    private String ReturnResult;
     private Toolbar toolbar;
-    //douglas
 
 
     private ActionBar actionBar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+
+    //webserivce
+//    public static String URL="http://localhost:63048/WebService1.asmx?WSDL";
+    //   public static String NAMESPACE="http://localhost:63048/";
+
 
 
 
@@ -50,9 +68,8 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 
-
-         toolbar = (Toolbar) findViewById(R.id.toolbar);
-      setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
         navigationView.setNavigationItemSelectedListener(this);
@@ -61,7 +78,7 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
         actionBar = getSupportActionBar();
 
         actionBar.setDisplayHomeAsUpEnabled(true);
-         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
 
         actionBar.setTitle("Teste");
 
@@ -69,11 +86,8 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
         campoEmail = (EditText) findViewById(R.id.campoEmail);
         campoSenha = (EditText) findViewById(R.id.campoSenha);
 
-        Button botaoLogin = (Button)findViewById(R.id.botaoLogin);
+        Button botaoLogin = (Button) findViewById(R.id.botaoLogin);
         botaoLogin.setOnClickListener(this);
-
-
-
 
 
     }
@@ -82,13 +96,64 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
     public void onClick(View v) {
         email = campoEmail.getText().toString();
         senha = campoSenha.getText().toString();
-        if (email.isEmpty() || senha.isEmpty()) {
-            Toast.makeText(Login.this, "Digite e-mail e senha", Toast.LENGTH_LONG).show();
-        }else {
-            Intent Myhome = new Intent(Login.this, MainActivity.class);
-            Myhome.putExtra("GetEmail", email);
-            startActivity(Myhome);
-        }
+
+
+        Thread  nt= new Thread(){
+
+            @Override
+            public void run(){
+                 String res;
+                Toast.makeText(Login.this, "EU to aquix", Toast.LENGTH_LONG).show();
+                String URL = "http://wcfservice120190319081657.azurewebsites.net/WebService1.asmx";
+                String NAMESPACE = "http://apphair.com/";
+                String SOAP_ACTION_LOGIN = "http://apphair.com/Cadastro";
+                String METHOD_NAME_LOGIN = "Cadastro";
+
+                SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME_LOGIN);
+                request.addProperty("email", email);
+                request.addProperty("senha", senha);
+
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                envelope.dotNet=true;
+
+                envelope.setOutputSoapObject(request);
+
+                HttpTransportSE transporte = new HttpTransportSE(URL);
+                res=null;
+
+                try
+                {
+                    transporte.call(SOAP_ACTION_LOGIN, envelope);
+                    SoapPrimitive resultado_xml = (SoapPrimitive) envelope.getResponse();
+                    res = resultado_xml.toString();
+                    Toast.makeText(Login.this, "EU to aqui", Toast.LENGTH_LONG).show();
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Toast.makeText(Login.this, "EU to aqui2", Toast.LENGTH_LONG).show();
+                } catch (XmlPullParserException e) {
+                    Toast.makeText(Login.this, "EU to aqui3", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Toast.makeText(Login.this, "EU to aqui4", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+                final String finalRes = res;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(Login.this, "EU to aqui", Toast.LENGTH_LONG).show();
+                     //   Toast.makeText(Login.this, finalRes, Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+
+            }
+
+
+
+        };
 
     }
 
@@ -114,7 +179,10 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
                 startActivity(intent2);
                 return true;
         }
-            return false;
-        }
+        return false;
     }
 
+
+
+
+}
