2ff16957f28d0c5e1fb428a7e37a933787abad9c
Gabriel Hofman <gabriel.hofman@hotmail.com>
2019-06-02 04:08:09 -0300

    test2

--- app/src/main/java/com/example/apphairnew/ui/Login.java
+++ app/src/main/java/com/example/apphairnew/ui/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -37,8 +38,9 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
 
 
     private ActionBar actionBar;
-    private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
 
 
     private String email, senha;
@@ -115,6 +117,18 @@ public class Login extends AppCompatActivity implements  NavigationView.OnNaviga
 
         }
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==android.R.id.home)
+        {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
